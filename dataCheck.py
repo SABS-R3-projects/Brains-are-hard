369a07b41b248d1b28887906da88198fc1b11b63
alisterde <alister.dale-evans@dtc.ox.ac.uk>
2020-02-14 11:46:47 +0000

    responsible reasAndSeperate function :panda_face:

--- dataCheck.py
+++ dataCheck.py
@@ -2,57 +2,91 @@ import nibabel as nib
 import os
 import numpy as np
 
-# load fmri images for patient 5 finger foot lip movements
-brains = os.path.join('data', 'sub-05', 'ses-retest', 'func', 'sub-05_ses-retest_task-fingerfootlips_bold.nii.gz')
-img = nib.load(brains)
-# turn the images into a 'data' format
-brainsdata = img.get_fdata()
-print('img.shape: ', img.shape)
 
-# separate the first four entries as these correspond to images
-# before a movement starts from the bulk of images
-nothing = brainsdata[:,:,:,0:4]
-print('nothing.shape', nothing.shape)
-brainsdata180 = brainsdata[:,:,:,4:]
-print('brainsdata180: ', brainsdata180.shape)
+def discardBadFrames(n, ofStart: int = 2, ofEnd: int = 0):
+    '''
+    Removes unwanted frames from the beginning and end of the fMRI 
+    '''
+    dims = n.shape
+    print('dims[-1]: ',dims[-1])
+    return n[:,:,:,:,ofStart:(dims[-1]-ofEnd)]
 
 def chunks(lst, n):
-    """Yield successive n-sized chunks from the numpy array lst based on the last dimesions index."""
-    dims = lst.shape
-    for i in range(0, dims[-1], n):
-        yield lst[:,:,:,i:i + n]
+        """Yield successive n-sized chunks from the numpy array lst based on the last dimesions index."""
+        dims = lst.shape
+        temp = []
+        for i in range(0, dims[-1]+1-n, n):
+            temp.append(lst[:,:,:,i:i + n])
+        return temp
 
-# grouping data into chunks of 6 images corresponding to 30 second intervals
-# TODO: probably change this to 3 and sperate movement from 'nothing'(no movement) that
-#  separates the movements
-brainsdatachunks = list(chunks(brainsdata180,6))
 
-brainsdatchunks = np.asarray(brainsdatachunks)
-# FIXME: This isn't the expected size probably somthing
-print('brains: ', brainsdatchunks.shape)
+def readAndSeparate(Filepath: str, chunksize: int = 6, ofStart: int = 2, ofEnd: int = 0):
+    '''
+    return : fingershrunk, footshrunk, lipsshrunk, nothingshrunk
+    '''
 
-# grouping the chunks into arrays by type of movement
-# TODO: separate out no movement
-# FIXME: These currently aren't the expected size
+    img = nib.load(Filepath)
+    # turn the images into a 'data' format
+    brainsdata = img.get_fdata()
+    #print('img.shape: ', img.shape)
 
-x = 0
-finger = []
-foot = []
-lips = []
+    # separate the first four entries as these correspond to images
+    # before a movement starts from the bulk of images
+    firstnothing = brainsdata[:,:,:,0:4]
+    #print('nothing.shape', nothing.shape)
+    brainsdata180 = brainsdata[:,:,:,4:]
+    #print('brainsdata180: ', brainsdata180.shape)
 
-for i in brainsdatachunks:
-    if x % 3 == 0:
-        finger.append(i)
-        #print('i: ', i.shape)
-        x += 1
-    if x % 3 == 1:
-        foot.append(i)
-       # print('i: ', i.shape)
-        x += 1
-    if x % 3 == 2:
-        lips.append(i)
-       # print('i: ', i.shape)
-        x += 1
+    # grouping data into chunks of 4 images corresponding to 15 second intervals
+    # and separate movement from 'nothing'(no movement) that
+    # separates the movements
+    brainsdatachunks = chunks(brainsdata180,chunksize)
+    brainsdatchunks = np.asarray(brainsdatachunks)
+    #print('brainsdatchunks: ', brainsdatchunks.shape)
 
-finger = np.asarray(finger)
-print('finger: ', finger.shape)
+    # grouping the chunks into arrays by type of movement
+
+    finger = []
+    foot = []
+    lips = []
+    nothinglst =[]
+    dims = brainsdatchunks.shape
+    #print('dims[0]: ',dims[0])
+
+
+    for i in range(0,dims[0]):
+
+        temp = brainsdatchunks[i,:,:,:]
+
+        if i % chunksize == 0:
+            finger.append(temp)
+            #print('i: ', i.shape)
+        if i % chunksize == 2:
+            foot.append(temp)
+        # print('i: ', i.shape)
+        if i % chunksize == 4:
+            lips.append(temp)
+        # print('i: ', i.shape)
+        else:
+            nothinglst.append(temp)
+        # print('i: ', i.shape)
+
+    # converting to numpy arrays
+    finger = np.asarray(finger)
+    foot = np.asarray(foot)
+    lips = np.asarray(lips)
+    nothing = np.asarray(nothinglst)
+    #print('nothing: ', nothing.shape)
+
+    fingershrunk = discardBadFrames(finger, ofStart, ofEnd)
+    footshrunk= discardBadFrames(foot, ofStart, ofEnd)
+    lipsshrunk = discardBadFrames(lips, ofStart, ofEnd)
+    nothingshrunk = discardBadFrames(nothing, ofStart, ofEnd)
+
+    return fingershrunk, footshrunk, lipsshrunk, nothingshrunk
+
+if __name__ == "__main__":
+    # load fmri images for patient 5 finger foot lip movements
+    Filepath = os.path.join('data', 'sub-05', 'ses-retest', 'func', 'sub-05_ses-retest_task-fingerfootlips_bold.nii.gz')
+    fing, foot, lip, nothing = readAndSeparate(Filepath)
+    print(fing.shape)
