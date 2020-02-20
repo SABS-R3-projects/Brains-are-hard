02de591688c409d18a8307e156a8b17492db49bc
Simon Marchant <simon.marchant@linacre.ox.ac.uk>
2020-02-20 15:12:56 +0000

    latest plotter for cat accuracy

--- code/image-classifier.py
+++ code/image-classifier.py
@@ -20,14 +20,16 @@ def build_model():
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=2))
 
-    model.add(Conv2D(32, 3))
+    model.add(Conv2D(64, 3))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, 3))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=2))
 
     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
     model.add(Dense(64))
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.2)) # gets rid of this % of nodes
     model.add(Dense(3)) # 3 different classes
     model.add(Activation('softmax')) # better than sigmoid for classification
 
@@ -38,7 +40,8 @@ def build_model():
     return model
 
 def train_model(model):
-    batch_size = 16
+    batch_size = 32
+    num_epochs = 100
 
     npz_file = np.load('training_data.npz')
     ylabel = npz_file['labels']
@@ -48,7 +51,9 @@ def train_model(model):
         xdata,
         ylabel,
         batch_size=batch_size,
-        epochs=10)
+        epochs=num_epochs,
+        validation_split=0.2
+        )
 
     npz_file = np.load('testing_data.npz')
     test_y = npz_file['labels']
@@ -58,12 +63,18 @@ def train_model(model):
 
     auc = metrics.roc_auc_score(test_y, pred_y)
     print('auc is ' + str(auc))
-
+    print('history keys are ' + str(history.history.keys()))
     pyplot.plot(history.history[acc_metric])
-    #pyplot.plot(history.history['mse'])
-    pyplot.ylabel('auc')
+    pyplot.plot(history.history['val_loss'])
+    pyplot.plot(history.history['loss'])
+    pyplot.ylabel('acc')
     pyplot.xlabel('epoch')
+    pyplot.xlim(0,num_epochs)
+    pyplot.ylim(0)
+    pyplot.title('Categorical Accuracy & loss for classifier')
+    pyplot.legend(['training accuracy metric', 'validation loss', 'training loss'], loc='upper right')
     pyplot.show()
+    pyplot.savefig('Cat acc loss.png')
 
 #    model.save_weights('weights/first_try.h5')
 
